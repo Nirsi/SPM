e8d9d8b6b212bed992a93eebabed00630f2dc9b2
Jan Novák <novak@silen.org>
2020-03-29 20:34:17 +0200

    calling new methods

---
+++ SPM/Router.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using SPM.PluginManagement;
+
+namespace SPM
+{
+    public static class Router
+    {
+        public static void ProcessInput(string[] args)
+        {
+            //if (CheckInput(args)) return;
+            switch (args[0])
+            {
+                case "search":
+                    Utils.ListSearchedPlugins(args[1]);
+                    break; 
+                
+                case "install":
+                    //Look for plugin and install it
+                    break;
+                
+                case "remove":
+                    //Remove plugin
+                    break;
+                
+                case "list":
+                    Utils.ListInstalledPlugins();
+                    break;
+            }
+        }
+
+        //This tries to catch bad input
+        private static bool CheckInput(string[] input)
+        {
+            if (input.Length < 1)
+            {
+                Console.WriteLine("No input params");
+                return true;
+            }
+            
+            if(input.Length < 2)
+            {
+                Console.WriteLine("missing second parameter");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
